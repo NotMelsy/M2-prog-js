bcdfaa19e762c70527148711dce10b7f54f58cce
Melle Siegers <34574@ma-web.nl>
2022-11-21 10:52:50 +0100

    html pagina

---
+++ les1/app.js
@@ -0,0 +1,25 @@
+class App
+{    
+    runApplication()
+    {
+         console.log ("hello world!");
+         let appNaam = "Melle"
+         let versienummer = "1.18.1"
+         let versiedatum = "21-11-2022"
+         let autheur = "Melle Siegers"
+         let copyright = "Melle studios"
+         let distributeur = "Jari lakeman"
+         let darkmode = true
+         
+         console.log(appNaam);
+         console.log(versienummer);
+         console.log(versiedatum);
+         console.log(autheur);
+         console.log(copyright);
+         console.log(distributeur);
+         console.log(darkmode);
+    }
+}
+console.log()
+ let app = new App();
+app.runApplication();
